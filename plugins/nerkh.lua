f65d3b9a3660d8d54a6d95fbc5eacbc46bc47716
5620g <abolfazlsaedi99@gmail.com>
2016-06-27 11:58:45 -0700

    Create nerkh.lua

---
+++ plugins/nerkh.lua
@@ -0,0 +1,23 @@
+do
+
+function run(msg, matches)
+  return [[
+نرخ گروه های مانستر تی جی
+لیست قیمت ها:
+http://s7.picofile.com/file/8239656634/Untitled.png
+
+برای خرید
+@tahajk (انهایی که ریپورت نیستند)
+  ]]
+
+  end
+return {
+  description = "shows nerkh", 
+  usage = "nerkh : return sudousers",
+  patterns = {
+    "^[Nn]erkh$",
+
+  },
+  run = run
+}
+end
