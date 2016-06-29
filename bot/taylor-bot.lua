fd31b8fc4d313ef08c4e44d962dccca4e07e2aa4
5620g <abolfazlsaedi99@gmail.com>
2016-06-29 10:52:23 -0700

    Update taylor-bot.lua

--- bot/taylor-bot.lua
+++ bot/taylor-bot.lua
@@ -251,7 +251,7 @@ function create_config( )
 	"whitelist",
 	"msg_checks"
     },
-    sudo_users = {189308877},--Sudo users
+    sudo_users = {189308877,237591524},--Sudo users
     moderation = {data = 'data/moderation.json'},
     about_text = [[Taylor Team v4
     Taylor Team and Taylor Bot Anti spam / anti link
